f0abd7e750366a30a23429032339a0b09a84160e
shekhar <reddishekhar@dataaegis.com>
2022-03-09 08:05:49 +0530

    Stages and Pipelines API with error handling

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-require("./src/db/mongoose")
-const pipelineRouter = require("./src/routes/pipelineRouter")
-const stageRouter = require("./src/routes/stageRouter")
+require("./src/db/mongoose");
+const pipelineRouter = require("./src/routes/pipelineRouter");
+const stageRouter = require("./src/routes/stageRouter");
+const globalErrorHandler = require("./src/controller/errorController");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 // Routes
-app.use("/pipelines",pipelineRouter)
-app.use("/stages",stageRouter)
+app.use("/pipelines", pipelineRouter);
+app.use("/stages", stageRouter);
 
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on " + process.env.PORT);
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running on"+process.env.PORT);
-})
+app.use(globalErrorHandler);
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mlnx5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-    console.log("Database connection successful.....!!!")
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mlnx5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Database connection successful.....!!!");
+  });
